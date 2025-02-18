b89524b1de7a16c6e811e8f009db2994bcad2d1c
Wittepoes1 <muld0066@hz.nl>
2025-02-18 14:43:20 +0100

    Fixed both the index and css files

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title')</title>
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/main.css') }}" rel="stylesheet">
     <link rel="icon" href="https://i.ibb.co/42rjFJg/3dgifmaker88668.gif" type="image/x-icon">
 </head>
 <body>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,4 +1,4 @@
-@extends('layout')
+@extends('components.layout')
 
 @section('title', 'Welcome to Scott\'s Website')
 
